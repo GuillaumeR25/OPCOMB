f31ffb7c93b9d2ea376241c7192510ff5491c509
tuveuxuncoca <adl.rlc95@gmail.com>
2018-02-05 11:41:43 +0100

    Import

--- src/Main/Model.java
+++ src/Main/Model.java
@@ -1,4 +1,7 @@
 package Main;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.constraints.Constraint;
+import org.chocosolver.solver.variables.IntVar;
 
 public class Model {
 	// nouveau modele
