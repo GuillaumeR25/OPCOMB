30630ea65f09ab645bbb4d45a69d4212e974edc8
tuveuxuncoca <adl.rlc95@gmail.com>
2018-02-05 11:59:38 +0100

    Test

--- src/Main/Model.java
+++ src/Main/Model.java
@@ -1,9 +1,5 @@
 package Main;
-import org.chocosolver.solver.Solution;
-import org.chocosolver.solver.constraints.Constraint;
-import org.chocosolver.solver.variables.IntVar;
 
 public class Model {
-	// nouveau modele
 
 }
