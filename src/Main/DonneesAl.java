8e80db7ab8a82a635af0a140f0fa4a374233d95a
tuveuxuncoca <adl.rlc95@gmail.com>
2018-02-13 16:07:45 +0100

    Test

--- src/Main/DonneesAl.java
+++ src/Main/DonneesAl.java
@@ -34,27 +34,6 @@ public class DonneesAl {
 	}
 
 
-
-	public static ArrayList<Navire> dNav(){
-		
-		ArrayList<Navire> rep = new ArrayList<>();
-		Navire n1 = new Navire(1, 250, 4, 100);
-		Navire n2 = new Navire(2, 2500, 7, 100);
-		Navire n3 = new Navire(3, 1200, 6, 300);
-		Navire n4 = new Navire(4, 130, 2, 500);
-		Navire n5 = new Navire(5, 1780, 4, 1200);
-		Navire n6 = new Navire(6, 670, 5, 1300);
-		Navire n7 = new Navire(7, 344, 3, 1600);
-		rep.add(n1);
-		rep.add(n2);
-		rep.add(n3);
-		rep.add(n4);
-		rep.add(n5);
-		rep.add(n6);
-		rep.add(n7);
-		return rep;
-	
-	}
 	
 	public static void main(String[] args) {
 		DonneesAl d = new DonneesAl(6);
